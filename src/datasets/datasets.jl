3944a1713f92572398dc4777fd7b6c41b8009d6f
Seth Bromberger <github@bromberger.com>
2016-08-31 08:43:28 -0700

    move smallgraphs back to LightGraphs

--- src/datasets/datasets.jl
+++ src/datasets/datasets.jl
@@ -3,13 +3,10 @@ module Datasets
 
 using ...LightGraphs
 using MatrixDepot
-# smallgraphs
-export smallgraph,
 
 # matrixdepot
-MDGraph, MDDiGraph
+export MDGraph, MDDiGraph
 
-include("smallgraphs.jl")
 include("matrixdepot.jl")
 
 end #module
--- test/datasets/runtests.jl
+++ test/datasets/runtests.jl
@@ -1,5 +1,4 @@
 tests = [
-    "datasets/smallgraphs",
     "datasets/matrixdepot"
 ]
 
