3f534f53ce6f17c65180f505176b5ad24c321a54
CarloLucibello <carlo.lucibello@gmail.com>
2016-09-19 12:24:32 +0200

    add minimal_weigth_perfect_matching based on BlossomV

--- test/matching/runtests.jl
+++ test/matching/runtests.jl
@@ -1,7 +1,3 @@
-using Base.Test
-using LightGraphs
-using LightGraphsExtras.Matching
-
 g =CompleteBipartiteGraph(2,2)
 w =Dict{Edge,Float64}()
 w[Edge(1,3)] = 10.
@@ -10,14 +6,10 @@ w[Edge(2,3)] = 2.
 w[Edge(2,4)] = 11.
 match = Matching.maximum_weight_maximal_matching(g,w)
 @test match.weight == 21
-@test match.inmatch[Edge(1,3)] == true
-@test match.inmatch[Edge(1,4)] == false
-@test match.inmatch[Edge(2,3)] == false
-@test match.inmatch[Edge(2,4)] == true
-@test match.m[1] == 3
-@test match.m[3] == 1
-@test match.m[2] == 4
-@test match.m[4] == 2
+@test match.mate[1] == 3
+@test match.mate[3] == 1
+@test match.mate[2] == 4
+@test match.mate[4] == 2
 
 g =CompleteBipartiteGraph(2,4)
 w =Dict{Edge,Float64}()
@@ -27,18 +19,10 @@ w[Edge(2,3)] = 11
 w[Edge(2,4)] = 1
 match = Matching.maximum_weight_maximal_matching(g,w)
 @test match.weight == 11.5
-@test match.inmatch[Edge(1,3)] == false
-@test match.inmatch[Edge(1,4)] == true
-@test match.inmatch[Edge(2,3)] == true
-@test match.inmatch[Edge(2,4)] == false
-@test match.inmatch[Edge(2,5)] == false
-@test match.inmatch[Edge(2,6)] == false
-@test match.inmatch[Edge(1,5)] == false
-@test match.inmatch[Edge(1,6)] == false
-@test match.m[1] == 4
-@test match.m[4] == 1
-@test match.m[2] == 3
-@test match.m[3] == 2
+@test match.mate[1] == 4
+@test match.mate[4] == 1
+@test match.mate[2] == 3
+@test match.mate[3] == 2
 
 g =CompleteBipartiteGraph(2,6)
 w =Dict{Edge,Float64}()
@@ -50,18 +34,10 @@ w[Edge(2,5)] = -1
 w[Edge(2,6)] = -1
 match = Matching.maximum_weight_maximal_matching(g,w,0)
 @test match.weight == 11.5
-@test match.inmatch[Edge(1,3)] == false
-@test match.inmatch[Edge(1,4)] == true
-@test match.inmatch[Edge(2,3)] == true
-@test match.inmatch[Edge(2,4)] == false
-@test match.inmatch[Edge(2,5)] == false
-@test match.inmatch[Edge(2,6)] == false
-@test match.inmatch[Edge(1,5)] == false
-@test match.inmatch[Edge(1,6)] == false
-@test match.m[1] == 4
-@test match.m[4] == 1
-@test match.m[2] == 3
-@test match.m[3] == 2
+@test match.mate[1] == 4
+@test match.mate[4] == 1
+@test match.mate[2] == 3
+@test match.mate[3] == 2
 
 g =CompleteBipartiteGraph(4,2)
 w =Dict{Edge,Float64}()
@@ -73,16 +49,75 @@ w[Edge(1,5)] = -1
 
 match = Matching.maximum_weight_maximal_matching(g,w,0)
 @test match.weight == 12
-@test match.inmatch[Edge(3,5)] == false
-@test match.inmatch[Edge(3,6)] == false
-@test match.inmatch[Edge(2,5)] == true
-@test match.inmatch[Edge(1,5)] == false
-@test match.inmatch[Edge(4,5)] == false
-@test match.inmatch[Edge(4,6)] == false
-@test match.inmatch[Edge(1,6)] == true
-@test match.m[1] == 6
-@test match.m[2] == 5
-@test match.m[3] == -1
-@test match.m[4] == -1
-@test match.m[5] == 2
-@test match.m[6] == 1
+@test match.mate[1] == 6
+@test match.mate[2] == 5
+@test match.mate[3] == -1
+@test match.mate[4] == -1
+@test match.mate[5] == 2
+@test match.mate[6] == 1
+
+w = Dict(Edge(1,2)=> 500)
+g =Graph(2)
+add_edge!(g,1,2)
+match = minimum_weight_perfect_matching(g, w)
+@test match.mate[1] == 2
+
+
+w=Dict( Edge(1,2)=>500,
+        Edge(1,3)=>600,
+        Edge(2,3)=>700,
+        Edge(3,4)=>100,
+        Edge(2,4)=>1000)
+
+g = CompleteGraph(4)
+match = minimum_weight_perfect_matching(g, w)
+@test match.mate[1] == 2
+@test match.mate[2] == 1
+@test match.mate[3] == 4
+@test match.mate[4] == 3
+@test match.weight == 600
+
+w = Dict(
+        Edge(1, 2) => 500,
+        Edge(1, 3) => 400,
+        Edge(2, 3) => 300,
+        Edge(3, 4) => 1000,
+        Edge(2, 4) => 1000
+    )
+g = CompleteGraph(4)
+match = minimum_weight_perfect_matching(g, w)
+@test match.mate[1] == 3
+@test match.mate[2] == 4
+@test match.mate[3] == 1
+@test match.mate[4] == 2
+@test match.weight == 1400
+
+g =CompleteBipartiteGraph(2,2)
+w =Dict{Edge,Float64}()
+w[Edge(1,3)] = -10
+w[Edge(1,4)] = -0.5
+w[Edge(2,3)] = -11
+w[Edge(2,4)] = -1
+
+match = Matching.minimum_weight_perfect_matching(g, w)
+@test match.mate[1] == 4
+@test match.mate[4] == 1
+@test match.mate[2] == 3
+@test match.mate[3] == 2
+@test match.weight == -11.5
+
+
+g =CompleteGraph(4)
+w =Dict{Edge,Float64}()
+w[Edge(1,3)] = 10
+w[Edge(1,4)] = 0.5
+w[Edge(2,3)] = 11
+w[Edge(2,4)] = 2
+w[Edge(1,2)] = 100
+
+match = Matching.minimum_weight_perfect_matching(g, w, 50)
+@test match.mate[1] == 4
+@test match.mate[4] == 1
+@test match.mate[2] == 3
+@test match.mate[3] == 2
+@test match.weight == 11.5
