5687734cabd696d9620fad31e7cffef36d39969d
mburq <burq.maximilien@gmail.com>
2017-02-19 12:06:00 -0500

    added test cases for missing edge weights, fixed docstring

--- src/matching/maximum_weight_matching.jl
+++ src/matching/maximum_weight_matching.jl
@@ -3,7 +3,7 @@ maximum_weight_matching{T <:Real}(g::Graph, w::Dict{Edge,T} = Dict{Edge,Int64}()
 
 Given a graph `g` and an edgemap `w` containing weights associated to edges,
 returns a matching with the maximum total weight.
-`w` is a dictionnary that maps edges i => j to weights.
+`w` is a dictionary that maps edges i => j to weights.
 If no weight parameter is given, all edges will be considered to have weight 1
 (results in max cardinality matching)
 
@@ -17,12 +17,12 @@ The package JuMP.jl and one of its supported solvers is required.
 Returns MatchingResult containing:
   - a solve status (indicating whether the problem was solved to optimality)
   - the optimal cost
-  - a list of each node's match.
+  - a list of each vertex's match (or -1 for unmatched vertices)
 """
 function maximum_weight_matching end
 
 function maximum_weight_matching{T <:Real}(g::Graph,
-          w::Dict{Edge,T} = Dict{Edge,Int64}(i => 1 for i in edge_list))
+          w::Dict{Edge,T} = Dict{Edge,Int64}(i => 1 for i in collect(edges(g))))
 
     model = Model()
     n = nv(g)
@@ -34,7 +34,7 @@ function maximum_weight_matching{T <:Real}(g::Graph,
         w[Edge(j,i)] = w[Edge(i,j)]
       end
     end
-    
+
     if setdiff(edge_list, keys(w)) != [] # If some edges do not have a key in w.
       error("Some edge weights are missing, check that keys i => j in w satisfy i <= j")
     end
